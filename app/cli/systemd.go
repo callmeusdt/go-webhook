c54c375aadb813b52c6de5582522793b68eb55bf
jojo <jojo@mbp>
2023-05-22 04:34:41 +0800

    adding cli command for config file generation

--- app/cli/systemd.go
+++ app/cli/systemd.go
@@ -12,7 +12,7 @@ func DumpSystemd() {
 	fmt.Printf("# /lib/systemd/system/%s.service\n", name)
 	fmt.Printf("# systemctl enable %s\n\n", name)
 	fmt.Printf("[Unit]\nDescription=%s for github\nAfter=network.target\n\n"+
-		"[Service]\nType=simple\nExecStart=/opt/gohook/hook -c %s\n#Restart=always\nUser=root\nWorkingDirectory=/opt/gohook\n# RuntimeDirectoryMode=2755\n\n"+
+		"[Service]\nType=simple\nExecStart=/opt/webhook/hook -c %s\n#Restart=always\nUser=root\nWorkingDirectory=/opt/webhook\n# RuntimeDirectoryMode=2755\n\n"+
 		"[Install]\nWantedBy=multi-user.target\nAlias=%s.service\n", name, app.OPTIONS.Config, name)
 
 }
