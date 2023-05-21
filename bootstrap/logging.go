e379e0725d4aa3761a0002fa01108a8bdd275b1f
jojo <jojo@mbp>
2023-05-22 05:27:40 +0800

    log updated

--- bootstrap/logging.go
+++ bootstrap/logging.go
@@ -7,8 +7,18 @@ import (
 )
 
 func InitLogger(appName string) {
+	dirname := "./log"
+	_, err := os.Stat(dirname)
+	if os.IsNotExist(err) {
+		os.MkdirAll(dirname, 0755)
+		fmt.Printf("Directory '%s' created\n", dirname)
+	} else if err != nil {
+		log.Fatalf("Error occurred while checking directory: %s, error: %w", dirname, err)
+	}
+
+	fileName := fmt.Sprintf("%s/%s.log", dirname, appName)
 	LogFile, err := os.OpenFile(
-		fmt.Sprintf("./log/%s.log", appName),
+		fileName,
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644,
 	)
 	if err != nil {
